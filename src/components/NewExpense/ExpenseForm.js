0f69de51ed7db80785c0cd1ee4c9a9f5baf6cf9e
Shobhit Saini <sainishobhit24@gmail.com>
2022-08-13 20:33:34 +0530

    expense form bug removed

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -45,9 +45,9 @@ const ExpenseForm = (props) => {
 
         const expenseData = {
             title: enteredTitle,
-            amount: enteredAmount,
-            date: new Date(enteredDate)
-        }
+            amount: +enteredAmount,
+            date: new Date(enteredDate),
+          };
 
         props.onSaveExpenseData(expenseData)
         //console.log(expenseData)
